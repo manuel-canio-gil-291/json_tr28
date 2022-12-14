44305b1aa33873c05804584afa0c305ec3169013
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-12-14 13:18:50 +0100

    Data Input

---
+++ src/main/java/es/mcg/input/data/Pass.java
@@ -0,0 +1,18 @@
+package es.mcg.input.data;
+
+public class Pass {
+    private Recipient recipient;
+
+    public Pass() {
+    }
+
+    public Recipient getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(Recipient recipient) {
+        this.recipient = recipient;
+    }
+
+    
+}
---
+++ src/main/java/es/mcg/input/data/Recipient.java
@@ -0,0 +1,18 @@
+package es.mcg.input.data;
+
+public class Recipient {
+    private String name;
+
+    public Recipient() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
